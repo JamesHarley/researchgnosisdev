357774551b198ff6dc957fbc3dea77798d08c151
Jah Mez <jahmez@Jahs-MacBook-Pro.local>
2018-08-01 23:48:52 -0400

    first

---
+++ footer.php
@@ -0,0 +1,35 @@
+			<!-- footer -->
+			<footer class="footer" role="contentinfo">
+
+				
+
+			</footer>
+			<!-- /footer -->
+
+		</div>
+		<!-- /wrapper -->
+
+		<?php wp_footer(); ?>
+		</div>
+		<div id="TheFoot">
+			Gnosis - the knowledge gained from experience, <br /> not theory or estimation
+			<p>
+				<a href="https://github.com/JamesHarley" target="_blank"><i class="fab fa-github-square"></i> GitHub </a>    
+				<a href="https://play.google.com/store/apps/developer?id=Gnosis+Development" target="_blank"><i class="fab fa-google-play"></i> GooglePlay </a>  
+				<a href="https://www.linkedin.com/in/james-harley-gnosis/" target="_blank"><i class="fab fa-linkedin"></i> Linkedin</a>
+			</p>
+			<p><a href="https://gnosisdevelopment.formstack.com/forms/bug_report" target="_blank">Found a bug?</a></p>
+		
+
+		<!-- analytics -->
+		<script>
+		(function(f,i,r,e,s,h,l){i['GoogleAnalyticsObject']=s;f[s]=f[s]||function(){
+		(f[s].q=f[s].q||[]).push(arguments)},f[s].l=1*new Date();h=i.createElement(r),
+		l=i.getElementsByTagName(r)[0];h.async=1;h.src=e;l.parentNode.insertBefore(h,l)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		ga('create', 'UA-XXXXXXXX-XX', 'yourdomain.com');
+		ga('send', 'pageview');
+		</script>
+
+	</body>
+</html>
